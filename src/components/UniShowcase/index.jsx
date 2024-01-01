2fe99a5fba7ef9e078dbad84dcec93e0ff84c747
Jasurbek <jasurdev1604@gmail.com>
2024-01-01 20:16:35 +0500

    Showcase

---
+++ src/components/UniShowcase/index.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Container } from "./style";
+
+const UniShowcase = (props) => {
+  return (
+    <Container className="container">
+      <div className="wrapper">
+        <Container.Title>UniShowcase</Container.Title>
+      </div>
+    </Container>
+  );
+};
+
+export default UniShowcase;
---
+++ src/components/UniShowcase/style.js
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+import uniShow from "../../assets/images/uniShow.png";
+
+export const Container = styled.div`
+  background: url(${uniShow});
+  background-repeat: no-repeat;
+  height: 310px;
+`;
+
+Container.Title = styled.div`
+  color: #fff;
+  font-size: 83px;
+  font-style: normal;
+  font-weight: 600;
+  line-height: normal;
+`;

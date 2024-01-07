9900aa97ea228566a5d0c005617cdf0b30c64da1
Jasurbek <jasurdev1604@gmail.com>
2024-01-07 07:01:13 +0500

    rectorat

--- src/pages/Faculties/index.jsx
+++ src/pages/Faculties/index.jsx
@@ -1,10 +1,78 @@
 import React from "react";
+import { Content } from "./style";
+import fak from "../../assets/icons/fakdark.png";
+import back from "../../assets/images/bggggg.png";
+import FacultyCart from "../../components/FacultyCart";
 
 const Faculties = () => {
+  const arr = [
+    {
+      id: 0,
+      img: fak,
+      title: "Transport tizimlari boshqaruvi fakulteti",
+      bg: back,
+    },
+    {
+      id: 1,
+      img: fak,
+      title: "Transport tizimlari boshqaruvi fakulteti",
+      bg: back,
+    },
+    {
+      id: 2,
+      img: fak,
+      title: "Transport tizimlari boshqaruvi fakulteti",
+      bg: back,
+    },
+    {
+      id: 3,
+      img: fak,
+      title: "Transport tizimlari boshqaruvi fakulteti",
+      bg: back,
+    },
+    {
+      id: 4,
+      img: fak,
+      title: "Transport tizimlari boshqaruvi fakulteti",
+      bg: back,
+    },
+    {
+      id: 5,
+      img: fak,
+      title: "Transport tizimlari boshqaruvi fakulteti",
+      bg: back,
+    },
+    {
+      id: 6,
+      img: fak,
+      title: "Transport tizimlari boshqaruvi fakulteti",
+      bg: back,
+    },
+    {
+      id: 7,
+      img: fak,
+      title: "Transport tizimlari boshqaruvi fakulteti",
+      bg: back,
+    },
+    {
+      id: 8,
+      img: fak,
+      title: "Transport tizimlari boshqaruvi fakulteti",
+      bg: back,
+    },
+  ];
+
   return (
     <div className="container">
       <div className="wrapper">
-        <h1>Faculties</h1>
+        <Content data-aos="fade-up">
+          <div className="title">Fakultetlar</div>
+          <Content.Body>
+            {arr.map(({ id, img, title, bg }) => {
+              return <FacultyCart img={img} title={title} back={bg} key={id} />;
+            })}
+          </Content.Body>
+        </Content>
       </div>
     </div>
   );
--- src/pages/Rectorat/index.jsx
+++ src/pages/Rectorat/index.jsx
@@ -1,10 +1,73 @@
 import React from "react";
+import RectorCart from "../../components/RectorCart";
+import { Content } from "./style";
+import rector from "../../assets/images/rector.png";
 
 const Rectorat = () => {
+  const arr = [
+    {
+      id: 0,
+      img: rector,
+      name: "Abduraxmanov Odil Kalandarovich",
+      position: "Rector",
+      desc: "Rektor, Iqtisodiyot fanlari doktori, Professor",
+      phone: ["(101) 222 123 456", "(101) 222 123 456"],
+      email: ["company@saas.com", "support@saas-stakk.com"],
+    },
+    {
+      id: 1,
+      img: rector,
+      name: "Abduraxmanov Odil Kalandarovich",
+      position: "Rector",
+      desc: "Rektor, Iqtisodiyot fanlari doktori, Professor",
+      phone: ["(101) 222 123 456", "(101) 222 123 456"],
+      email: ["company@saas.com", "support@saas-stakk.com"],
+    },
+    {
+      id: 2,
+      img: rector,
+      name: "Abduraxmanov Odil Kalandarovich",
+      position: "Rector",
+      desc: "Rektor, Iqtisodiyot fanlari doktori, Professor",
+      phone: ["(101) 222 123 456", "(101) 222 123 456"],
+      email: ["company@saas.com", "support@saas-stakk.com"],
+    },
+    {
+      id: 3,
+      img: rector,
+      name: "Abduraxmanov Odil Kalandarovich",
+      position: "Rector",
+      desc: "Rektor, Iqtisodiyot fanlari doktori, Professor",
+      phone: ["(101) 222 123 456", "(101) 222 123 456"],
+      email: ["company@saas.com", "support@saas-stakk.com"],
+    },
+    {
+      id: 4,
+      img: rector,
+      name: "Abduraxmanov Odil Kalandarovich",
+      position: "Rector",
+      desc: "Rektor, Iqtisodiyot fanlari doktori, Professor",
+      phone: ["(101) 222 123 456", "(101) 222 123 456"],
+      email: ["company@saas.com", "support@saas-stakk.com"],
+    },
+    {
+      id: 5,
+      img: rector,
+      name: "Abduraxmanov Odil Kalandarovich",
+      position: "Rector",
+      desc: "Rektor, Iqtisodiyot fanlari doktori, Professor",
+      phone: ["(101) 222 123 456", "(101) 222 123 456"],
+      email: ["company@saas.com", "support@saas-stakk.com"],
+    },
+  ];
   return (
     <div className="container">
       <div className="wrapper">
-        <h1>Rectorat</h1>
+        <Content data-aos="fade-up">
+          {arr.map((item) => (
+            <RectorCart data={item} key={item.id} />
+          ))}
+        </Content>
       </div>
     </div>
   );
